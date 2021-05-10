a83e3285800dd8d6eadafd3b87ebe6ebbd30eb18
Stephen Obie <sobie31@outlook.com>
2021-05-09 21:00:23 -0400

    trying to fix js, added some css

--- script.js
+++ script.js
@@ -1,36 +1,50 @@
-var forecastApi = 'https://api.openweathermap.org/data/2.5/forecast?q='
-var Url = `https://api.openweathermap.org/data/2.5/weather?q=`
+// var forecastApi = 'https://api.openweathermap.org/data/2.5/forecast?q='
+// var Url = `https://api.openweathermap.org/data/2.5/weather?q=`
 
-function weatherBallon( cityName ) {
-    var key = '&appid=88107b5d4c713e0e883f6b636fc103a2';
-    fetch('https://api.openweathermap.org/data/2.5/forecast?q='  + key)
-     .then(function(resp) {return resp.json() })
-     .then(function(data) {
-         console.log(data);
-     })
-     .catch(function() {
+// function weatherBallon( cityName ) {
+//     var key = '&appid=88107b5d4c713e0e883f6b636fc103a2';
+//     fetch('https://api.openweathermap.org/data/2.5/forecast?q='  + key)
+//      .then(function(resp) {return resp.json() })
+//      .then(function(data) {
+//          console.log(data);
+//      })
+//      .catch(function() {
 
-     });
-}
-window.onload = function() {
-    weatherBallon( 6167865 );
-}
-function drawWeather( d ) {
-    var fahrenheit = math.round(((parseFloat(d.main.temperature)-273.15)*1.8)+32);
+//      });
+// }
+// window.onload = function() {
+//     weatherBallon( 6167865 );
+// }
+// function drawWeather( d ) {
+//     var fahrenheit = math.round(((parseFloat(d.main.temperature)-273.15)*1.8)+32);
 
-    document.getElementById('description').innerHTML = d.weather[0].description;
-    document.getElementById('temperature').innerHTML = fahrenheit + '&deg;';
-    document.getElementById('location').innerHTML = d.name;
-}
+//     document.getElementById(description).innerHTML = d.weather[0].description;
+//     document.getElementById(temperature).innerHTML = fahrenheit + '&deg;';
+//     document.getElementById(location).innerHTML = d.name;
+// }
 
-function weatherBallon( cityName ) {
-    var key = '{88107b5d4c713e0e883f6b636fc103a2}';
-    fetch(forecastApi + key)
-     .then(function(resp) {return resp.json() })
-     .then(function(data) {
-         drawWeather(data);
-     })
-     .catch(function() {
+// function weatherBallon( cityName ) {
+//     var key = '88107b5d4c713e0e883f6b636fc103a2';
+//     fetch(forecastApi + cityName + '&appids=' + key)
+//      .then(function(resp) {return resp.json() })
+//      .then(function(data) {
+//          drawWeather(data);
+//      })
+//      .catch(function() {
 
-     });
-}
+//      });
+// }
+
+var apiKey = '&appid=88107b5d4c713e0e883f6b636fc103a2';
+var inputVal = input.Value;
+
+var url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+
+fetch(url)
+.then(response => response.json())
+.then(data => {
+
+})
+.catch(() => {
+    msg.textContent = "Search for valid city";
+});
